8379e2461481e66ef44a0f8435fdb6957dae0fbc
Jason Pan <jasonpan@jasons-mbp.nyhq.nytint.com>
2016-07-14 14:19:49 -0400

    Quick setup

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-This is a test
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+	return 'Hello World'
+
+if __name__ = '__main__':
+	app.run(debug=True)
