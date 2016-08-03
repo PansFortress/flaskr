861e57bc571a8e40ecb63affae6cf149c24fafb2
= <Japan8789@gmail.com>
2016-08-03 15:08:04 -0400

    PEP8 changes

--- app.py
+++ app.py
@@ -5,25 +5,27 @@ from forms import NameForm
 
 
 def create_app():
-	app = Flask(__name__)
-	app.config['SECRET_KEY'] = 'some random string'
-	Bootstrap(app)
-	#CsrfProtect(app)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'some random string'
+    Bootstrap(app)
+    # CsrfProtect(app)
 
-	return app
+    return app
 
 app = create_app()
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-	return render_template("base.html")
 
-@app.route('/form', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("base.html")
+
+
+@app.route('/form', methods=['GET', 'POST'])
 def form():
-	form = NameForm()
-	if form.validate_on_submit():
-		return redirect(url_for('index'))
-	return render_template("base_form.html", form=form)
+    form = NameForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+        return render_template("base_form.html", form=form)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
